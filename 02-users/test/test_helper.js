0065c0055167211c1f26b9e42177b218d22730bb
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-15 22:03:01 +0200

    02-Setup mongoose to connect to mongoDB

---
+++ 02-users/test/test_helper.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/users_test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('Good to go!'))
