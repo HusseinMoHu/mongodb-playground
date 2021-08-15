77425c0c195a01bffe08680a0186e5dc84877451
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-15 22:05:57 +0200

    02-First Mocha test

---
+++ 02-users/test/create_test.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+const User = require('../src/user')
+
+describe('Create records', () => {
+  it('saves a user', () => {
+    const hatem = new User({
+      name: 'hatem'
+    })
+    hatem.save()
+  })
+})
