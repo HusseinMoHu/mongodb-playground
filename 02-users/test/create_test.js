c77e1da9342b22c1ff94f8ddc43713042a9147d1
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-16 01:24:47 +0200

    02-Mongoose's isNew property

--- 02-users/test/create_test.js
+++ 02-users/test/create_test.js
@@ -2,10 +2,15 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe('Create records', () => {
-  it('saves a user', () => {
+  it('saves a user', (done) => {
     const hatem = new User({
       name: 'hatem',
     })
-    hatem.save()
+
+    hatem.save().then(() => {
+      // Has hatem been saved successfully?
+      assert(!hatem.isNew)
+      done()
+    })
   })
 })
