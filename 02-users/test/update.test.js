2ec91b39ec68b012fa888d1ca89599243f032493
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-18 17:12:49 +0200

    02-instance updateOne

--- 02-users/test/update.test.js
+++ 02-users/test/update.test.js
@@ -9,15 +9,22 @@ describe('Updating records', () => {
     hatem.save().then(() => done())
   })
 
-  it('instance based set n save', (done) => {
-    hatem.set({ name: 'Hossam' })
-    hatem
-      .save()
+  const assertName = (operation, done) => {
+    operation
       .then(() => User.find({}))
       .then((users) => {
         assert(users.length === 1)
         assert(users[0].name === 'Hossam')
         done()
       })
+  }
+
+  it('instance based set n save', (done) => {
+    hatem.set({ name: 'Hossam' }) // Change it in memory NOT in Database, so no async needed!
+    assertName(hatem.save(), done)
+  })
+
+  it('instance method updateOne', (done) => {
+    assertName(hatem.updateOne({ name: 'Hossam' }), done)
   })
 })
