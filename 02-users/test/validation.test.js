d99cd521fb8bc05da6cd197f525d41a70d6e99ec
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-19 19:07:25 +0200

    02-Mongoose - validation with a validator function

--- 02-users/test/validation.test.js
+++ 02-users/test/validation.test.js
@@ -2,13 +2,21 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe('Validating records', () => {
-  it('requires a username', (done) => {
+  it('requires a username', () => {
     const user = new User({ name: undefined })
 
     const validationResult = user.validateSync()
     const { message } = validationResult.errors.name
 
     assert(message === 'Name is required.')
-    done()
+  })
+
+  it('Should username be longer than 2 characters', () => {
+    const user = new User({ name: 'An' })
+
+    const validationResult = user.validateSync()
+    const { message } = validationResult.errors.name
+
+    assert(message === 'Name must be longer than 2 characters.')
   })
 })
