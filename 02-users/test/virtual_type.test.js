7d9044726f94c2083e838da2ddd8c8a90c964e62
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-21 00:36:44 +0200

    02-Trigger virtual-type test

---
+++ 02-users/test/virtual_type.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+const User = require('../src/user')
+
+describe('Virtual types', () => {
+  it('postCount return number of posts', (done) => {
+    const hatem = new User({
+      name: 'Hatem',
+      posts: [{ title: 'First Post' }, { title: 'Second Post' }],
+    })
+
+    hatem
+      .save()
+      .then(() => User.findOne({ name: 'Hatem' }))
+      .then((user) => {
+        assert(user.postCount.length === 2)
+        done()
+      })
+  })
+})
