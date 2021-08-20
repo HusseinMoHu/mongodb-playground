a0b649669f38c7dd5797168e8f22b3403bea486b
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-20 23:33:42 +0200

    02-Removing subdocument

--- 02-users/test/subdocument.test.js
+++ 02-users/test/subdocument.test.js
@@ -30,4 +30,25 @@ describe('Subdocuments', () => {
         done()
       })
   })
+
+  it('can remove an existing subdocument', (done) => {
+    const hatem = new User({ name: 'Hatem', posts: [{ title: 'New Title' }] })
+
+    hatem
+      .save()
+      .then(() => User.findOne({ name: 'Hatem' }))
+      .then((user) => {
+        const post = user.posts[0]
+        // remove() -> When it comes to subdocument, it's not async operation
+        // it's not gonna saves the document to Database
+        post.remove()
+
+        return user.save()
+      })
+      .then(() => User.findOne({ name: 'Hatem' }))
+      .then((user) => {
+        assert(user.posts.length === 0)
+        done()
+      })
+  })
 })
