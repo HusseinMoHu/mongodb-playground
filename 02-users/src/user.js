91b291a463ae542c4ea18e447687e379308e324a
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-22 15:55:12 +0200

    02-mongoose middlewares & dealing with Cyclic-Requires

--- 02-users/src/user.js
+++ 02-users/src/user.js
@@ -24,6 +24,15 @@ userSchema.virtual('postCount').get(function () {
   return this.posts.length
 })
 
+userSchema.pre('remove', async function () {
+  const user = this
+
+  // to Dealing with cyclic Requires
+  // mongoose.model() call will not be invoked until this function actually runs
+  // as opposite to require() which grabbing model before everything else
+  const BlogPost = mongoose.model('BlogPost')
+})
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
