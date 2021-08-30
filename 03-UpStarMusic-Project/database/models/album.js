9a7e67f77e616222f015055abde1e13fe122abbe
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-30 12:56:07 +0200

    03-CRUD Operations

--- 03-UpStarMusic-Project/database/models/album.js
+++ 03-UpStarMusic-Project/database/models/album.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-const albumSchema = new mongoose.Schema({
-    title: String,
-    date: Date,
-    copiesSold: Number,
-    numberTracks: Number,
-    image: String,
-    revenue: Number
+const AlbumSchema = new mongoose.Schema({
+  title: String,
+  date: Date,
+  copiesSolid: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number,
 })
 
-module.exports = albumSchema
+module.exports = AlbumSchema
--- 03-UpStarMusic-Project/database/models/artist.js
+++ 03-UpStarMusic-Project/database/models/artist.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose')
-const albumSchema = require('./album')
+const AlbumSchema = require('./album')
 
-const artistSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    yearsActive: Number,
-    image: String,
-    genre: String,
-    website: String,
-    netWorth: Number,
-    labelName: String,
-    retired: Boolean,
-    albums: [albumSchema]
+const ArtistSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema],
 })
 
-const Artist = mongoose.model('Artist', artistSchema)
+const Artist = mongoose.model('Artist', ArtistSchema)
+
+module.exports = Artist
